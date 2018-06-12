01f790dfb6a331ea69ada2b9bdf84957b3f08c2e
Luke Rigby <1016498+its-rigs@users.noreply.github.com>
2018-06-12 11:48:51 +1000

    moved zsh to stand alone dep

--- brew-apps.rb
+++ brew-apps.rb
@@ -3,7 +3,6 @@ brew_apps = %w{
   htop
   tig
   tldr
-  zsh
 }
 
 brew_apps.each do |app|
@@ -20,10 +19,17 @@ dep 'blackbox.bin' do
   provides 'blackbox_initialize'
 end
 
+dep 'zsh.bin do
+  met? {
+    shell('$(brew --prefix zsh)').p.exists?
+  }
+end
+
 dep 'install all brew apps' do
   requires [
     *(brew_apps).map { |a| "#{a}.bin" },
     # 'neovim.bin',
-    'blackbox.bin'
+    'blackbox.bin',
+    'zsh.bin'
   ]
 end
