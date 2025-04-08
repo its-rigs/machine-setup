4e0fdfaa952848ff856739f5ee12237101957bde
Luke Rigby <1016498+its-rigs@users.noreply.github.com>
2025-04-08 11:01:52 +1000

    Add nvim config

---
+++ dotfiles/nvim/chadrc.lua
@@ -0,0 +1,5 @@
+local M = {}
+M.ui = { theme = 'pastelDark' }
+M.plugins = 'custom.plugins'
+
+return M
--- scripts/dots.sh
+++ scripts/dots.sh
@@ -16,8 +16,25 @@ ln -sf "$working_dir/git/gitconfig" ~/.gitconfig
 ln -sf "$working_dir/git/gitignore" ~/.gitignore
 
 print_subtask "Link zsh files"
-ln -sf "$working_dir/application-files/ohmyposh-theme.toml" ~/.config/ohmyposh-theme.toml
+ln -sf "$working_dir/dotfiles/ohmyposh-theme.toml" ~/.config/ohmyposh-theme.toml
 ln -sf "$working_dir/shell/zshrc" ~/.zshrc
 
 print_subtask "Link tmux files"
-ln -sf "$working_dir/application-files/tmux.conf" ~/.tmux.conf
+ln -sf "$working_dir/dotfiles/tmux.conf" ~/.tmux.conf
+
+
+# ------------------------------------------------------------------------------
+# SETUP NVIM
+# ------------------------------------------------------------------------------
+print_task "Setup Nvim"
+
+print_subtask "Cleanup any existing Nvim state"
+rm -rf ~/.config/nvim
+rm -rf ~/.local/state/nvim
+rm -rf ~/.local/share/nvim
+
+print_subtask "Download NvChad"
+git clone https://github.com/NvChad/starter ~/.config/nvim --depth 1
+
+print_subtask "Link nvim files"
+ln -sf "$working_dir/dotfiles/nvim" ~/.config/nvim/lua/custom
