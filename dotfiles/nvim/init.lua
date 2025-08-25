144cecc023f34f1f6cdb4705aebff407904bb806
Luke Rigby <1016498+its-rigs@users.noreply.github.com>
2025-08-25 21:40:32 +1000

    Add treesitter

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -17,7 +17,8 @@ local plugins = {
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
      dependencies = { 'nvim-lua/plenary.nvim' }
-  }
+  },
+  {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"}
 }
 local opts = {}
 
