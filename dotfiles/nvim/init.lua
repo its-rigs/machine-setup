944fc5f009aeabfb904ccae7240eb1011f4426e1
Luke Rigby <1016498+its-rigs@users.noreply.github.com>
2025-08-30 12:17:35 +1000

    Reorganise all the things

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -1,33 +1,22 @@
 -- Install lazy
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-     dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"}
-}
-local opts = {}
-
+-- Set options
 require("vim-options")
-require("lazy").setup(plugins, opts)
 
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-
-require("catppuccin").setup()
-vim.cmd.colorscheme = "catppuccin"
+-- Load plugins
+require("lazy").setup("plugins")
---
+++ dotfiles/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", -- optional, but recommended
+    },
+    lazy = false,
+    config = function()
+      vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal left toggle<CR>")
+      vim.keymap.set("n", "<leader>fe", ":Neotree filesystem focus<CR>")
+    end,
+  },
+}
